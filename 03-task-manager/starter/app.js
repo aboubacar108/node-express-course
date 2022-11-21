995395dbbaef3918aec29dabce5041da67efa1ab
aboubacar108 <108593484+aboubacar108@users.noreply.github.com>
2022-11-21 10:40:46 -0500

    Update app.js
    console.log(`server is listening on port ${port}...`) wrapped in a callback.

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -22,7 +22,9 @@ const port = 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
-    app.listen(port, console.log(`server is listening on port ${port}...`))
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}...`)
+    }) 
   } catch(error) {
     console.log(error)
   }
